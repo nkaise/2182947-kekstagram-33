a01aa006474c351a99c47e2cd908c926cb23c6f8
Anastasiia <52383976+nkaise@users.noreply.github.com>
2024-10-29 13:35:00 +0400

    Добавляет исправления 4 домашнего задания

--- js/main.js
+++ js/main.js
@@ -10,8 +10,29 @@ const DESCRIPTION = [
   'Дети играют на пляже у воды.',
   'Пустыня с одиноким кактусом на закате.'
 ];
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const NAMES = [
+  'Александр',
+  'Екатерина',
+  'Дмитрий',
+  'Анна',
+  'Сергей',
+  'Ольга',
+  'Михаил',
+  'Мария',
+  'Илья',
+  'Наталья'
+];
 let currentIdIndex = -1;
 let currentUrlIndex = -1;
+let currentCommentIndex = -1;
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -20,30 +41,39 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-const checkForDuplicateIds = () => {
-  const idsArray = [];
-  const generateId = (a, b) => {
-    const id = getRandomInteger(1, 25);
-    if (!idsArray.includes(id)) {
-      idsArray.push(id);
+const checkForDuplicateIds = (min, max) => {
+  const idArray = [];
+  const generateId = () => {
+    const id = getRandomInteger(min, max);
+    if (!idArray.includes(id)) {
+      idArray.push(id);
     }
   };
-  while (idsArray.length !== 25) {
-    generateId(1, 25);
+  while (idArray.length !== max) {
+    generateId(min, max);
   }
-  return idsArray;
+  return idArray;
 };
 
-const idArray = checkForDuplicateIds();
-const urlArray = checkForDuplicateIds();
+const idsArray = checkForDuplicateIds(1,25);
+const urlsArray = checkForDuplicateIds(1,25);
+const commentsArray = checkForDuplicateIds(1,1000);
 
 const createPhotoPost = () => ({
-  id: `${idArray[currentIdIndex++]}`,
-  url: `photos/${urlArray[currentUrlIndex++]}.jpg`,
+  id: `${idsArray[currentIdIndex++]}`,
+  url: `photos/${urlsArray[currentUrlIndex++]}.jpg`,
   description: DESCRIPTION[`${getRandomInteger(0,9)}`],
-  likes: `${[getRandomInteger(15,200)]}`
+  likes: `${[getRandomInteger(15,200)]}`,
+  comments: [{
+    id: `${commentsArray[currentCommentIndex++]}`,
+    avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
+    message: MESSAGE[`${getRandomInteger(0,5)}`],
+    name: NAMES[`${getRandomInteger(0,9)}`]
+  }]
 });
 
 createPhotoPost();
 
 const similarWizards = Array.from({length: 25}, createPhotoPost);
+
+console.log(similarWizards);
