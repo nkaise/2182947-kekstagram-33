6ee7a3fed00a9416c920136609820307b920a2b3
Anastasiia <52383976+nkaise@users.noreply.github.com>
2024-11-16 13:49:59 +0400

    8.15. Открывается и закрывается (часть 2)

--- js/fullscreen-image-viewer.js
+++ js/fullscreen-image-viewer.js
@@ -1,4 +1,5 @@
 import {clearInnerElements, isEscapeKey} from './utils';
+import {COMMENTS_COUNT_STEP} from './photo-data';
 
 const similarCommentFragment = document.createDocumentFragment();
 const bigPictureContainer = document.querySelector('.big-picture');
@@ -8,9 +9,11 @@ const bigPictureCloseElement = bigPictureContainer.querySelector('#picture-cance
 const bigPictureElement = bigPictureContainer.querySelector('.big-picture__img img');
 const likesCountElement = bigPictureContainer.querySelector('.likes-count');
 const totalCommentsElement = bigPictureContainer.querySelector('.social__comment-total-count');
+const shownCommentsElement = bigPictureContainer.querySelector('.social__comment-shown-count');
 const photoDescriptionElement = bigPictureContainer.querySelector('.social__caption');
 const commentsCountElement = bigPictureContainer.querySelector('.social__comment-count');
 const commentsLoaderElement = bigPictureContainer.querySelector('.comments-loader');
+let currentComments = [];
 
 const onBigPictureEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -26,10 +29,37 @@ function closeBigPicture (pictureContainer, commentsContainer) {
   document.removeEventListener('keydown', onBigPictureEscKeydown);
 }
 
-const openBigPicture = (pictureContainer, commentsAmount, commentsLoaderButton) => {
+const createPostComments = (comment) => {
+  const commentElement = socialCommentElement.cloneNode(true);
+  commentElement.querySelector('.social__picture').src = comment.avatar;
+  commentElement.querySelector('.social__text').alt = comment.name;
+  commentElement.querySelector('.social__text').textContent = comment.message;
+  similarCommentFragment.append(commentElement);
+};
+
+let totalComments = COMMENTS_COUNT_STEP;
+const renderPostComments = () => {
+  socialCommentsContainer.textContent = '';
+  totalComments = totalComments > currentComments.length ? currentComments.length : totalComments;
+  const showPartComments = currentComments.slice(0, totalComments);
+  for (let i = 0; i < showPartComments.length; i++) {
+    createPostComments(showPartComments[i]);
+  }
+  if (totalComments === currentComments.length) {
+    commentsLoaderElement.classList.add('hidden');
+  }
+  socialCommentsContainer.append(similarCommentFragment);
+  shownCommentsElement.textContent = totalComments;
+};
+
+commentsLoaderElement.addEventListener('click', () => {
+  totalComments += 5;
+  renderPostComments();
+});
+
+const openBigPicture = (pictureContainer) => {
+  totalComments = COMMENTS_COUNT_STEP;
   pictureContainer.classList.remove('hidden');
-  commentsAmount.classList.add('hidden');
-  commentsLoaderButton.classList.add('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onBigPictureEscKeydown);
 };
@@ -38,25 +68,19 @@ bigPictureCloseElement.addEventListener('click', () => {
   closeBigPicture(bigPictureContainer, socialCommentsContainer);
 });
 
-const renderPostComments = (comments) => {
-  comments.forEach((comment) => {
-    const commentElement = socialCommentElement.cloneNode(true);
-    commentElement.querySelector('.social__picture').src = comment.avatar;
-    commentElement.querySelector('.social__text').alt = comment.name;
-    commentElement.querySelector('.social__text').textContent = comment.message;
-    similarCommentFragment.append(commentElement);
-  });
+const dataForBigPicture = (data) => {
+  bigPictureElement.src = data.url;
+  likesCountElement.textContent = data.likes;
+  totalCommentsElement.textContent = data.comments.length.toString();
+  photoDescriptionElement.textContent = data.description;
 };
 
 const renderBigPicture = (smallPicture) => {
-  openBigPicture(bigPictureContainer, commentsCountElement, commentsLoaderElement, socialCommentsContainer);
-  bigPictureElement.src = smallPicture.url;
-  likesCountElement.textContent = smallPicture.likes;
-  totalCommentsElement.textContent = smallPicture.comments.length.toString();
-  photoDescriptionElement.textContent = smallPicture.description;
+  currentComments = smallPicture.comments.slice();
+  openBigPicture(bigPictureContainer, commentsCountElement);
+  dataForBigPicture(smallPicture);
   clearInnerElements(socialCommentsContainer);
-  renderPostComments(smallPicture.comments);
-  socialCommentsContainer.append(similarCommentFragment);
+  renderPostComments();
 };
 
 export {renderBigPicture};
