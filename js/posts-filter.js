4810c71fb71d4694af74a62a7131573a9fb2a857
Anastasiia <52383976+nkaise@users.noreply.github.com>
2024-12-08 16:01:21 +0400

    Доработки posts-filter модуля

--- js/posts-filter.js
+++ js/posts-filter.js
@@ -1,37 +1,44 @@
 import {renderPosts} from './miniature-rendering';
+import {debounce} from './utils';
+import {RERENDER_DELAY} from './miniature-rendering-data';
 
+const picturesContainerElement = document.querySelector('.pictures');
 const filterElement = document.querySelector('.img-filters');
 const defaultFilterElement = filterElement.querySelector('#filter-default');
 const randomFilterElement = filterElement.querySelector('#filter-random');
 const filterDiscussedElement = filterElement.querySelector('#filter-discussed');
 
 const changeFilters = (currentFilter) => {
-  const activeButton = filterElement.querySelector('.img-filters__button--active');
-  if (activeButton) {
-    activeButton.classList.remove('img-filters__button--active');
+  const activeButtonElement = filterElement.querySelector('.img-filters__button--active');
+  if (activeButtonElement) {
+    activeButtonElement.classList.remove('img-filters__button--active');
   }
   currentFilter.classList.add('img-filters__button--active');
-  const currentPictures = document.querySelectorAll('.picture');
-  currentPictures.forEach((element) => element.remove());
+  const currentPicturesElement = document.querySelectorAll('.picture');
+  currentPicturesElement.forEach((element) => element.remove());
 };
+
+const debouncedRenderPosts = debounce((postsToRender) => {
+  renderPosts(picturesContainerElement, postsToRender);
+}, RERENDER_DELAY);
 
 const filterPosts = (evt, posts) => {
   const initialPostsList = posts;
-  const picturesContainerElement = document.querySelector('.pictures');
   if (evt.target.closest(`#${randomFilterElement.id}`)) {
     changeFilters(randomFilterElement);
-    const randomPosts = posts.toSorted(() => Math.random() - 0.5);
-    renderPosts(picturesContainerElement, randomPosts.slice(0,10));
+    const randomPosts = posts.toSorted(() => Math.random() - 0.5).slice(0, 10);
+    debouncedRenderPosts(randomPosts);
     return;
   }
   if (evt.target.closest(`#${defaultFilterElement.id}`)) {
     changeFilters(defaultFilterElement);
-    renderPosts(picturesContainerElement, initialPostsList);
+    debouncedRenderPosts(initialPostsList);
     return;
   }
   if (evt.target.closest(`#${filterDiscussedElement.id}`)) {
     changeFilters(filterDiscussedElement);
-    renderPosts(picturesContainerElement, posts.toSorted((a, b) => b.comments.length - a.comments.length));
+    const mostDiscussedPosts = posts.toSorted((a, b) => b.comments.length - a.comments.length);
+    debouncedRenderPosts(mostDiscussedPosts);
   }
 };
 
